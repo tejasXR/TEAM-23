e1c395a0fe761cb409594b5081d31ac63f2c5f4d
Tejas Shroff <tejasshroff24@gmail.com>
2022-03-24 19:38:03 -0400

    Adding basic Annotation class

---
+++ Assets/all1y Wizards/Scripts/Annotation.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Annotation : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    [SerializeField] private TextMeshProUGUI text;
+
+    public void Initialize(string annotationText, Vector3 position)
+    {
+        text.text = annotationText;
+        Position(position);
+    }
+    
+    public void Initialize(Sprite annotationSprite, Vector3 position)
+    {
+        image.sprite = annotationSprite;
+        Position(position);
+    }
+
+    private void Position(Vector3 position)
+    {
+        transform.position = position;
+    }
+}
