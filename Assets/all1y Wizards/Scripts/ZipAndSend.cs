d9c521bc1c128ad49eca6084414ef35b44585b88
Hung Nguyen <hungnguyen094@gmail.com>
2022-03-25 11:30:47 -0400

    starter zip & send code

---
+++ Assets/all1y Wizards/Scripts/ZipAndSend.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+public class ZipAndSend {
+    public void Zip(string toFilename, params string[] fromFilenames) {
+        using var stream = new FileStream(toFilename, FileMode.Create);
+        using var archive = new ZipArchive(stream, ZipArchiveMode.Create);
+        C
+        archive.CreateEntryFromFile()
+    }
+}
