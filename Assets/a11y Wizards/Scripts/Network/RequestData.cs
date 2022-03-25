9b60bd2f34306abace725c6ac0c72595a1f4ad76
Nhan Tran <megatran@dhcp-10-31-111-63.dyn.mit.edu>
2022-03-25 16:36:59 -0400

    feat(server): initial codebase to send requests to server

---
+++ Assets/a11y Wizards/Scripts/Network/RequestData.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class RequestData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       StartCoroutine(GetRequest("http://0.0.0.0:5000")); 
+    }
+
+    IEnumerator GetRequest(string uri) {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri)) {
+            yield return webRequest.SendWebRequest();
+            if (webRequest.isNetworkError) {
+                Debug.Log("Error: " + webRequest.error);
+            } else {
+                Debug.Log(webRequest.downloadHandler.text);
+            }
+        }
+    }
+}
