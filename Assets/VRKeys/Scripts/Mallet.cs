ecb9fb5af51914baadd1c35a54a12b516e67146a
Tejas Shroff <tejasshroff24@gmail.com>
2022-03-25 19:31:49 -0400

    getting some sort of VRKeys keyboard to show

--- Assets/VRKeys/Scripts/Mallet.cs
+++ Assets/VRKeys/Scripts/Mallet.cs
@@ -9,6 +9,8 @@
  */
 
 using UnityEngine;
+using UnityEngine.XR;
+using System.Collections;
 
 namespace VRKeys {
 
@@ -44,17 +46,7 @@ namespace VRKeys {
 
 		private void Awake () {
 			audioSource = GetComponent<AudioSource> ();
-			controller = gameObject.AddComponent<OculusController> ();
-
-			/*switch (VRSettings.loadedDeviceName) {
-				case "Oculus":
-					controller = gameObject.AddComponent<OculusController> ();
-					break;
-
-				case "OpenVR":
-					controller = gameObject.AddComponent<OpenVRController> ();
-					break;
-			}*/
+			controller = GetComponent<Controller> ();
 		}
 
 		private void FixedUpdate () {
@@ -70,6 +62,7 @@ namespace VRKeys {
 		/// </summary>
 		/// <param name="key">Key.</param>
 		public void HandleTriggerEnter (Key key) {
+			audioSource.Stop ();
 			audioSource.PlayOneShot (clipToPlay);
 
 			if (transform.parent.transform.parent != null) {
