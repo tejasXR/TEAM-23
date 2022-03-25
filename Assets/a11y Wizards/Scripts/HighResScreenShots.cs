3d18ab4eea98a5a66885656e15428327551a2ae7
Nhan Tran <megatran@dhcp-10-31-111-63.dyn.mit.edu>
2022-03-25 16:36:59 -0400

    feat(server): initial codebase to send requests to server

--- Assets/a11y Wizards/Scripts/HighResScreenShots.cs
+++ Assets/a11y Wizards/Scripts/HighResScreenShots.cs
@@ -39,6 +39,13 @@ namespace a11y_Wizards.Scripts
             }
         }*/
 
+        private void Update() {
+        if (Input.GetKeyUp("k"))
+            {
+                CaptureScreenshot();
+            }
+        }
+
         //Initialize Directory
         private void Start()
         {
@@ -50,89 +57,86 @@ namespace a11y_Wizards.Scripts
             }
         }
         
-        private string CreateFileName(int width, int height)
+        private string CreateFileName(string cameraName, int width, int height)
         {
             //timestamp to append to the screenshot filename
             string timestamp = DateTime.Now.ToString("yyyyMMddTHHmmss");
             // use width, height, and timestamp for unique file 
-            var filename = string.Format("{0}/screen_{1}x{2}_{3}.{4}", outputFolder, width, height, timestamp, format.ToString().ToLower());
-            // return filename
+            //var filename = string.Format("{0}/screen_{1}x{2}_{3}.{4}", outputFolder, width, height, timestamp, format.ToString().ToLower());
+            var filename = $"{timestamp}_{cameraName}_{width}x{height}";
             return filename;
-        }
-
-        public static string ScreenShotName(int width, int height)
-        {
-            var path = "C:/Users/tejas/Documents/Personal Projects/MIT Reality Hack 2022/TEAM-23/Assets";
-            return string.Format("{0}/screenshots/screen_{1}x{2}_{3}.png", path, width, height,
-                System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
         }
 
         private void CaptureScreenshot()
         {
             isProcessing = true;
             // create screenshot objects
-            if (renderTexture == null)
-            {
-                // creates off-screen render texture to be rendered into
-                rect = new Rect(0, 0, captureWidth, captureHeight);
-                renderTexture = new RenderTexture(captureWidth, captureHeight, 24);
-                screenShot = new Texture2D(captureWidth, captureHeight, TextureFormat.RGB24, false);
-            }
 
             // get main camera and render its output into the off-screen render texture created above
-            Camera camera = cameras[0];
-            camera.targetTexture = renderTexture;
-            camera.Render();
-            // mark the render texture as active and read the current pixel data into the Texture2D
-            RenderTexture.active = renderTexture;
-            screenShot.ReadPixels(rect, 0, 0);
-            // reset the textures and remove the render texture from the Camera since were done reading the screen data
-            camera.targetTexture = null;
-            RenderTexture.active = null;
-            // get our filename
-            string filename = CreateFileName((int) rect.width, (int) rect.height);
-            // get file header/data bytes for the specified image format
-            byte[] fileHeader = null;
-            byte[] fileData = null;
-            //Set the format and encode based on it
-            if (format == Format.RAW)
+            //Camera camera = cameras[0];
+            foreach (var camera in cameras)
             {
-                fileData = screenShot.GetRawTextureData();
-            }
-            else if (format == Format.PNG)
-            {
-                fileData = screenShot.EncodeToPNG();
-            }
-            else if (format == Format.JPG)
-            {
-                fileData = screenShot.EncodeToJPG();
-            }
-            else //For ppm files
-            {
-                // create a file header - ppm files
-                string headerStr = string.Format("P6\n{0} {1}\n255\n", rect.width, rect.height);
-                fileHeader = System.Text.Encoding.ASCII.GetBytes(headerStr);
-                fileData = screenShot.GetRawTextureData();
-            }
-
-            // create new thread to offload the saving from the main thread
-            new System.Threading.Thread(() =>
-            {
-                var file = System.IO.File.Create(filename);
-                if (fileHeader != null)
+                if (renderTexture == null)
                 {
-                    file.Write(fileHeader, 0, fileHeader.Length);
+                    // creates off-screen render texture to be rendered into
+                    rect = new Rect(0, 0, captureWidth, captureHeight);
+                    renderTexture = new RenderTexture(captureWidth, captureHeight, 24);
+                    screenShot = new Texture2D(captureWidth, captureHeight, TextureFormat.RGB24, false);
                 }
 
-                file.Write(fileData, 0, fileData.Length);
-                file.Close();
-                Debug.Log(string.Format("Screenshot Saved {0}, size {1}", filename, fileData.Length));
-                isProcessing = false;
-            }).Start();
-            //Cleanup
-            Destroy(renderTexture);
-            renderTexture = null;
-            screenShot = null;
+                camera.targetTexture = renderTexture;
+                camera.Render();
+                // mark the render texture as active and read the current pixel data into the Texture2D
+                RenderTexture.active = renderTexture;
+                screenShot.ReadPixels(rect, 0, 0);
+                // reset the textures and remove the render texture from the Camera since were done reading the screen data
+                camera.targetTexture = null;
+                RenderTexture.active = null;
+                // get our filename
+                string filename = CreateFileName(camera.name, (int)rect.width, (int)rect.height);
+                // get file header/data bytes for the specified image format
+                byte[] fileHeader = null;
+                byte[] fileData = null;
+                //Set the format and encode based on it
+                if (format == Format.RAW)
+                {
+                    fileData = screenShot.GetRawTextureData();
+                }
+                else if (format == Format.PNG)
+                {
+                    fileData = screenShot.EncodeToPNG();
+                }
+                else if (format == Format.JPG)
+                {
+                    fileData = screenShot.EncodeToJPG();
+                }
+                else //For ppm files
+                {
+                    // create a file header - ppm files
+                    string headerStr = string.Format("P6\n{0} {1}\n255\n", rect.width, rect.height);
+                    fileHeader = System.Text.Encoding.ASCII.GetBytes(headerStr);
+                    fileData = screenShot.GetRawTextureData();
+                }
+
+                // create new thread to offload the saving from the main thread
+                new System.Threading.Thread(() =>
+                {
+                    var file = System.IO.File.Create(filename);
+                    if (fileHeader != null)
+                    {
+                        file.Write(fileHeader, 0, fileHeader.Length);
+                    }
+
+                    file.Write(fileData, 0, fileData.Length);
+                    file.Close();
+                    Debug.Log(string.Format("Screenshot Saved {0}, size {1}", filename, fileData.Length));
+                    isProcessing = false;
+                }).Start();
+                //Cleanup
+                Destroy(renderTexture);
+                renderTexture = null;
+                screenShot = null;
+            }
         }
     }
 }
