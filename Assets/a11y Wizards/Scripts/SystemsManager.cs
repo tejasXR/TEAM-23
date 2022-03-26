2e143b898e712e11acfd8745ce353ef405bb2f07
Tejas Shroff <tejasshroff24@gmail.com>
2022-03-25 23:12:24 -0400

    More changes!

--- Assets/a11y Wizards/Scripts/SystemsManager.cs
+++ Assets/a11y Wizards/Scripts/SystemsManager.cs
@@ -11,22 +11,52 @@ public class SystemsManager : MonoBehaviour
     [SerializeField] private StreamingRecognizer streamingRecognizer;
     [SerializeField] private Keyboard keyboard;
 
+    private bool _flowStarted;
+
     /*private void Start()
     {
         StartAnnotationFlow();
     }*/
 
+    private void OnEnable()
+    {
+        streamingRecognizer.onInterimResult.AddListener(UpdateKeyboardDisplayText);
+    }
+
+    private void OnDisable()
+    {
+        streamingRecognizer.onInterimResult.RemoveListener(UpdateKeyboardDisplayText);
+    }
+
     public void StartAnnotationFlow()
     {
         StartCoroutine(EnableKeyboard());
+        _flowStarted = true;
     }
 
-    public void FeedbackSubmitted(string s)
+    public void UpdateKeyboardDisplayText(string s)
+    {
+        if (!keyboard.initialized) return;
+        if (!_flowStarted) return;
+        keyboard.SetText(s);
+    }
+
+    public void CancelFeedback()
     {
         keyboard.Disable();
-        annotationSystem.CreateAnnotation(s);
+        _flowStarted = false;
     }
-    
+
+    public void SubmitFeedback()
+    {
+        keyboard.Disable();
+        
+        var feedbackText = keyboard.displayText.text;
+        annotationSystem.CreateAnnotation(feedbackText);
+
+        _flowStarted = false;
+    }
+
     private IEnumerator EnableKeyboard()
     {
         yield return new WaitUntil(() => keyboard.initialized);
