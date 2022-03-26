d86b99700159cc85d3822d2b9219b040250a8d4e
Hung Nguyen <hungnguyen094@gmail.com>
2022-03-26 11:04:19 -0400

    added game logic

---
+++ Assets/a11y Wizards/Scripts/GameLogic/GameState.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameState {
+    private int total = 0;
+    private int done = 0;
+
+    public event System.Action GameEnded;
+    
+    public static GameState Instance { get; } = new GameState();
+
+    GameState() {
+        GameEnded += OnEnd;
+    }
+
+    public void AddTotalCounter() {
+        total++;
+    }
+
+    public void AddCompletedCounter() {
+        Debug.Log($"Done {done}/{total}");
+        done++;
+        if (done == total) {
+            GameEnded?.Invoke();
+        }
+    }
+
+    private void OnEnd() {
+        Debug.Log($"Game completed {done}/{total}");
+    }
+}
+
+public enum Category {
+    Blue,
+    Indigo,
+    Violet,
+}
